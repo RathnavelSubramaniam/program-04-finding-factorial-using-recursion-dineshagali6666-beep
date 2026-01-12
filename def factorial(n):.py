8ea094efcca627ea6ceb9796bd3ff8a1d1534f5d
dineshagali6666-beep <dineshagali6666@gmail.com>
2026-01-12 06:41:47 +0000

    program-4 completed

---
+++ def factorial(n):.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+           return n*factorial(n-1)
+number=int(input("enter a number:"))
+if number<0:
+    print("\n Error:Factorial is not defined for negative numbers. ")
+else:
+    result=factorial(number)
+    print(f"\nthe factorial of {number} is {result}")                   
